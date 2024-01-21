01310c434e64cab35a9b8f2c005fa4d01c8708f4
DAIORANGE <121008037@qq.com>
2024-01-21 08:19:41 +0800

    Update main.py
    加了多网站

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import json
 import os
 
 requests.packages.urllib3.disable_warnings()
+
+# 用于发送通知的变量
 SCKEY = os.environ.get('SCKEY')
 TG_BOT_TOKEN = os.environ.get('TGBOT')
 TG_USER_ID = os.environ.get('TGUSERID')
 
-
-def checkin(email=os.environ.get('EMAIL'), password=os.environ.get('PASSWORD'),
-            base_url=os.environ.get('BASE_URL'), ):
+def checkin(email, password, base_url):
     email = email.split('@')
     email = email[0] + '%40' + email[1]
     session = requests.session()
@@ -30,17 +30,25 @@ def checkin(email=os.environ.get('EMAIL'), password=os.environ.get('PASSWORD'),
                       'Chrome/56.0.2924.87 Safari/537.36',
         'Referer': base_url + '/user'
     }
-    response = session.post(base_url + '/user/checkin', headers=headers,
-                            verify=False)
+    response = session.post(base_url + '/user/checkin', headers=headers, verify=False)
     response = json.loads(response.text)
     print(response['msg'])
     return response['msg']
 
+# 签到多个网站
+sites = {
+    'BASE_URL': ('EMAIL', 'PASSWORD'),
+    'BASE_URL1': ('EMAIL1', 'PASSWORD1'),
+    'BASE_URL2': ('EMAIL2', 'PASSWORD2'),
+    # 可以继续添加更多的网站
+}
 
-result = checkin()
-if SCKEY != '':
-    sendurl = 'https://sctapi.ftqq.com/' + SCKEY + '.send?title=机场签到&desp=' + result
-    r = requests.get(url=sendurl)
-if TG_USER_ID != '':
-    sendurl = f'https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage?chat_id={TG_USER_ID}&text={result}&disable_web_page_preview=True'
-    r = requests.get(url=sendurl)
+for base_url, credentials in sites.items():
+    email, password = credentials
+    result = checkin(email, password, base_url)
+    if SCKEY != '':
+        sendurl = f'https://sctapi.ftqq.com/{SCKEY}.send?title=机场签到&desp={result}'
+        requests.get(url=sendurl)
+    if TG_USER_ID != '':
+        sendurl = f'https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage?chat_id={TG_USER_ID}&text={result}&disable_web_page_preview=True'
+        requests.get(url=sendurl)
