76d7176438a928e0892642d4754151f0a9356413
DAIORANGE <121008037@qq.com>
2024-01-21 08:24:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ TG_BOT_TOKEN = os.environ.get('TGBOT')
 TG_USER_ID = os.environ.get('TGUSERID')
 
 def checkin(email, password, base_url):
-    email = email.split('@')
-    email = email[0] + '%40' + email[1]
+    if '@' not in email:
+        print("Invalid email format for", email)
+        return "Invalid email format"
+
+    email = email.replace('@', '%40')
     session = requests.session()
     session.get(base_url, verify=False)
     login_url = base_url + '/auth/login'
@@ -37,18 +40,19 @@ def checkin(email, password, base_url):
 
 # 签到多个网站
 sites = {
-    'BASE_URL': ('EMAIL', 'PASSWORD'),
-    'BASE_URL1': ('EMAIL1', 'PASSWORD1'),
-    'BASE_URL2': ('EMAIL2', 'PASSWORD2'),
+    os.environ.get('BASE_URL'): (os.environ.get('EMAIL'), os.environ.get('PASSWORD')),
+    os.environ.get('BASE_URL1'): (os.environ.get('EMAIL1'), os.environ.get('PASSWORD1')),
+    os.environ.get('BASE_URL2'): (os.environ.get('EMAIL2'), os.environ.get('PASSWORD2')),
     # 可以继续添加更多的网站
 }
 
 for base_url, credentials in sites.items():
-    email, password = credentials
-    result = checkin(email, password, base_url)
-    if SCKEY != '':
-        sendurl = f'https://sctapi.ftqq.com/{SCKEY}.send?title=机场签到&desp={result}'
-        requests.get(url=sendurl)
-    if TG_USER_ID != '':
-        sendurl = f'https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage?chat_id={TG_USER_ID}&text={result}&disable_web_page_preview=True'
-        requests.get(url=sendurl)
+    if base_url and all(credentials):
+        email, password = credentials
+        result = checkin(email, password, base_url)
+        if SCKEY:
+            sendurl = f'https://sctapi.ftqq.com/{SCKEY}.send?title=机场签到&desp={result}'
+            requests.get(url=sendurl)
+        if TG_USER_ID:
+            sendurl = f'https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage?chat_id={TG_USER_ID}&text={result}&disable_web_page_preview=True'
+            requests.get(url=sendurl)
